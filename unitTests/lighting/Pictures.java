cd9136e3ee64d2d0523fda1fe03bd8d969a4f9fe
ליאל תהלה שמחי <lieltehi@g.jct.ac.il>
2022-06-02 22:28:39 +0300

    targil 8. working on picture

--- unitTests/lighting/Pictures.java
+++ unitTests/lighting/Pictures.java
@@ -26,17 +26,10 @@ public class Pictures {
                 //.setBackground(new Color(184,255,154))
                 .build();
 
-        Camera camera = new Camera(new Point(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+        Camera camera = new Camera(new Point(0, 0, 1300), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
                 .setVPSize(200, 200) //
                 .setVPDistance(1000);
 
-        Geometry sphere1 = new Sphere(new Point(0, 0, -200), 20d)
-                .setEmission(new Color(ORANGE).reduce(2))
-                .setMaterial(new Material().setkD(0.2).setkS(0.2).setShininess(30).setKt(0.6));
-//        Geometry sphere2 = new Sphere(new Point(25, -40, 25), 20d)
-//                .setEmission(new Color(75,255,250).reduce(2))
-//                .setMaterial(new Material().setkD(0.5).setkS(0.2).setShininess(200).setKt(0.4));
-//
 //        Geometry triangle1 = new Triangle(new Point(0,0,-20), new Point(30,50,-20), new Point(-30,50,-20))
 //                .setEmission(new Color(80,255,211)).setMaterial(new Material().setkD(0.2).setkS(0.5).setShininess(500).setKr(0.3));
 //        Geometry triangle2 = new Triangle(new Point(0,-25,-40), new Point(30,25,-40), new Point(-30,25,-40))
@@ -47,32 +40,38 @@ public class Pictures {
                 .setEmission(new Color(255,195,96));
         Geometry ceil = new Plane(new Point(0,100,0),new Vector(0,1,0)).setMaterial(new Material().setkD(1))
                 .setEmission(new Color(255,195,96));
-        Geometry frontWall = new Plane(new Point(0,0,-500),new Vector(0,0,1)).setMaterial(new Material().setkD(1))
+        Geometry frontWall = new Plane(new Point(0,0,-500),new Vector(0,0,1)).setMaterial(new Material().setKr(0.8).setkD(0.1))
+                .setEmission(new Color(BLACK));
+        Geometry besideWall = new Plane(new Point(0,0,1300),new Vector(0,0,1)).setMaterial(new Material().setkD(1))
                 .setEmission(new Color(255,195,96));
         Geometry rightWall = new Plane(new Point(90,0,0),new Vector(1,0,0)).setMaterial(new Material().setkD(1))
-                .setEmission(new Color(PINK));
+                .setEmission(new Color(RED));
         Geometry leftWall = new Plane(new Point(-90,0,0),new Vector(1,0,0)).setMaterial(new Material().setkD(1))
                 .setEmission(new Color(RED));
-        Geometry triangleMerrow1 = new Triangle(new Point(30,-50,-400), new Point(30,50,-500),new Point(90,50,-320))
-                .setMaterial(new Material().setkD(0.2).setKt(0.6).setkS(0.1)).setEmission(Color.BLACK);
-        Geometry triangleMerrow2 = new Triangle(new Point(80,-50,-300), new Point(40,-50,-450),new Point(90,50,-500))
-                .setMaterial(new Material().setkD(0.001).setKr(0.9).setkS(1)).setEmission(new Color(WHITE));
-//        Geometry table1 = new Triangle(new Point(0,0,-400), new Point(30,50,-400), new Point(-30,50,-20));
-////                .setEmission(new Color(80,255,211)).setMaterial(new Material().setkD(0.2).setkS(0.5).setShininess(500).setKr(0.3));
-        Geometry triangle1 = new Triangle(new Point(-25,98,-200), new Point(25,98,-200),new Point(0,98,-240))
-                .setMaterial(new Material().setkD(0.2).setKt(0.6).setkS(0.1));
-        scene.getGeometries().add(floor,ceil,frontWall,rightWall,leftWall,sphere1,triangle1,triangleMerrow2);
-        scene.getLights().add( //
-                new SpotLight(new Color(BLUE), new Point(0, 99, -205), new Vector(0, -1, 0)) //
-                        .setKl(4E-4).setKq(2E-5));
-//        scene.getLights().add(new SpotLight(new Color(221, 211, 255),new Point(80, -40, 500), new Vector(-3, 1, -4))
-//                .setKl(0.001)
-//                .setKq(0.0001));
-        scene.getLights().add(new PointLight(new Color(GREEN), new Point(0,90,45)).setKl(0.0001).setKq(0.00002));
+
+        Geometry sphere1 = new Sphere(new Point(-50, -30, -200), 20d)
+                .setEmission(new Color(RED).reduce(2))
+                .setMaterial(new Material().setkD(1));
+        Geometry sphere2 = new Sphere(new Point(25, -20, 25), 30d)
+                .setEmission(new Color(RED).reduce(2))
+                .setMaterial(new Material().setkD(1));
+        Geometry sphere3 = new Sphere(new Point(0,100,0), 20d)
+                .setEmission(new Color(PINK).reduce(2)) //blue
+                .setMaterial(new Material().setkD(1));
+
+        scene.getGeometries().add(floor,ceil,frontWall,besideWall,rightWall,leftWall,sphere1,sphere2,sphere3);
+//        scene.getLights().add( //
+//                new SpotLight(new Color(BLUE), new Point(0, 99, -205), new Vector(0, 1, 0)) //
+//                        .setKl(4E-4).setKq(2E-5));
+        scene.getLights().add(new SpotLight(new Color(GREEN),new Point(0, 80, 300), new Vector(0, -1, -5))
+               .setKl(0.0001)
+               .setKq(0.0001));
+        //scene.getLights().add(new PointLight(new Color(GREEN), new Point(0,90,50)).setKl(0.0001).setKq(0.0005));
+        //scene.getLights().add(new PointLight(new Color(RED), new Point(0,90,1100)).setKl(0.0001).setKq(0.00002));
 //        scene.getLights().add(new DirectionalLight(new Color(221,11,255), new Vector(50, -7, -0.5)));
 
 
-        ImageWriter imageWriter = new ImageWriter("OurFirstPictureShadow3", 500, 500);
+        ImageWriter imageWriter = new ImageWriter("OurProjectPicture", 500, 500);
         camera.setImageWriter(imageWriter) //
                 .setRayTracer(new RayTracerBasic(scene)) //
                 .renderImage() //
